9824bc6f29d7487402d7ef92e5fa1b8725139ec1
Joonas Salojärvi <salojarvi.joonas@gmail.com>
2019-03-30 18:49:58 +0200

    BeerInfoPage WIP

--- src/pages/beers/BeerInfo.jsx
+++ src/pages/beers/BeerInfo.jsx
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-import {Container, Form, InputGroup, Button, Col, Row} from 'react-bootstrap';
+import {Container, Form, InputGroup, Button, Col, Row, Media} from 'react-bootstrap';
 
 import axios from 'axios';
 
-import BeerCard from '../../components/BeerCard';
+import beerImg from '../../img/testbeer.png';
 
 import './BeerInfo.css';
 
@@ -11,21 +11,21 @@ class BeerInfo extends Component {
 
     constructor(props) {
         super(props);
-        
+        this.beerInfoColumn = this.beerInfoColumn.bind(this);
         this.state = {
-            listOfBeers: []
+            beer: []
         }
     }
 
     componentWillMount() {
         axios.get(
-            'beers/'
+            'beers/' + this.props.match.params.id
         )
         .then(response => {
             if(response.status === 200) {
-                this.setState({listOfBeers: response.data});
+                this.setState({beer: response.data[0]});
                 console.log(response.data);
-                console.log(this.state.listOfBeers);
+                console.log(this.state.beer);
             }
         })
         .catch(function(response) {
@@ -33,37 +33,33 @@ class BeerInfo extends Component {
         });
     }
 
+    beerInfoColumn(){
+      return (
+        <h1>{this.state.beer.beerName}</h1>
+      )
+    }
+
     render() {
-        const beerCards = this.state.listOfBeers.map((d) =>
-                <li className="beerItem">
-                    <BeerCard 
-                        beerName={d.beerName}
-                        description={d.description}
-                        alcoholPercent={d.alcoholPercent}
-                    />
-                </li>
-        );
+        
+        console.log(this.props.match.params.id);
 
         return (
           <Container>
             <div id="beersPage" className="rounded">
-              <h1 id="beersH1">Beers</h1>
-              <div id="beerSearch">
-                <Row className="justify-content-md-center">
-                  <Col id="searchColumn" xl={7} xs={12}>
-                    <InputGroup id="searchInputGroup">
-                      <Form.Control type="text" placeholder="TODO"></Form.Control>
-                      <Button>Search</Button>
-                    </InputGroup>
-                  </Col>
-                </Row>
-              </div>
-              <ul id="beerCardsList">
-                {beerCards}
-              </ul>
+            <Row className='justify-content-center'>
+              <Media>
+                <img
+                  width={100}
+                  height={100}
+                  src={beerImg}
+                  alt="Generic placeholder"
+                />
+              </Media>
+            </Row>
+            asdasd
             </div>
           </Container>
-        );
+        )
     }
 
 }
