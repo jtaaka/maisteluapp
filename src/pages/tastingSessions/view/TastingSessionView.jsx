57dcdd17e260ade5725fbcb6404c854cf40bd888
Teemu Salminen <teemu.salminen@cs.tamk.fi>
2019-04-03 22:18:16 +0300

    Tasting session view page ajax call

--- src/pages/tastingSessions/view/TastingSessionView.jsx
+++ src/pages/tastingSessions/view/TastingSessionView.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 
 import {Container} from 'react-bootstrap';
 
+import axios from 'axios';
+
 import './TastingSessionView.css';
 import User from '../../../User';
 
@@ -14,17 +16,36 @@ class TastingSessionView extends Component {
 
     this.state = {
       tastingSessionId: passedParameters.tastingSessionId != null ? passedParameters.tastingSessionId : -1,
-      user: passedParameters.user != null ? passedParameters.user : new User()
+      user: passedParameters.user != null ? passedParameters.user : new User(),
+      tastingSessionName: '',
+      tastingSessionStartingDate: new Date(),
+      tastingSessionAdditionalInfo: '',
+      tastingSessionBeers: []
     };
 
     console.log(this.state.tastingSessionId + " ID");
     console.log(this.state.user.username + " USER");
   }
 
+  componentWillMount() {
+    axios.get('tastingsession/' + this.state.tastingSessionId)
+    .then((response) => {
+        if(response.status === 200) {
+          this.setState({
+            tastingSessionName: response.data.name,
+            tastingSessionStartingDate: response.data.startingDate,
+            tastingSessionAdditionalInfo: response.data.tastingSessionAdditionalInfo,
+            tastingSessionBeers: response.data.beers
+          });
+        }
+    })
+    .catch(error => console.log(error));
+  }
+
   render() {
     return (
-      <Container>
-        <p>HELLO</p>
+      <Container id="tastingsession-container" className="rounded">
+        <h1>{this.state.tastingSessionName}</h1>
       </Container>
     );
   }
