cab702c709dbc3f823b293a81f41e8c4de35f8b1
lehdonen <36726382+lehdonen@users.noreply.github.com>
2019-04-09 21:28:40 +0300

    Add profile component

---
+++ src/pages/profile/profile.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {Container} from "react-bootstrap";
+
+
+class Profile extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Container>
+            </Container>
+        );
+    }
+
+}
+
+export default Profile;
+
