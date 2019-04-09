f867d1b9454f0feafa9abc73409b15bef5633109
Teemu Salminen <teemu.salminen@cs.tamk.fi>
2019-04-09 10:04:16 +0300

    Tastingsession view page WIP

--- src/pages/tastingSessions/view/TastingSessionView.jsx
+++ src/pages/tastingSessions/view/TastingSessionView.jsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
-
-import {Container} from 'react-bootstrap';
+import {Container, Row, Col, Button, Card} from 'react-bootstrap';
 
 import axios from 'axios';
+import moment from 'moment';
+
+import User from '../../../User';
+import {DATE_FORMAT} from '../../../GlobalConfig';
+
+import beerImg from '../../../img/testbeer.png';
 
 import './TastingSessionView.css';
-import User from '../../../User';
 
 class TastingSessionView extends Component {
 
@@ -20,11 +24,12 @@ class TastingSessionView extends Component {
       tastingSessionName: '',
       tastingSessionStartingDate: new Date(),
       tastingSessionAdditionalInfo: '',
-      tastingSessionBeers: []
+      tastingSessionBeers: [],
+      userJoinedInSession: false
     };
 
-    console.log(this.state.tastingSessionId + " ID");
-    console.log(this.state.user.username + " USER");
+    this.createRateBeerCard = this.createRateBeerCard.bind(this);
+    //this.createJoinOrQuitSessionButton = this.createJoinOrQuitSessionButton.bind(this);
   }
 
   componentWillMount() {
@@ -33,19 +38,70 @@ class TastingSessionView extends Component {
         if(response.status === 200) {
           this.setState({
             tastingSessionName: response.data.name,
-            tastingSessionStartingDate: response.data.startingDate,
-            tastingSessionAdditionalInfo: response.data.tastingSessionAdditionalInfo,
-            tastingSessionBeers: response.data.beers
+            tastingSessionStartingDate: new Date(response.data.startingDate),
+            tastingSessionAdditionalInfo: response.data.additionalInfo,
           });
         }
     })
     .catch(error => console.log(error));
   }
 
+  updateIsUserJoinedInSession() {
+    this.state.user.getIsUserJoinedInTastingSession
+    .then((response) => {
+      if(response.status === 200) {
+        this.setState({userJoinedInSession : true});
+      } else {
+        this.setState({userJoinedInSession : false});
+      }
+    })
+  }
+
+  createRateBeerCard(beer) {
+    return (
+      <Card className="shadow align-items-center">
+        <Card.Img variant="top" src={beerImg}  style={{ width: '13rem' }} />
+        <Card.Body>
+          <Card.Title>Olvi3</Card.Title>
+          <Card.Text>Saatana</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  }
+
   render() {
+
+    let beerCard = this.createRateBeerCard(0);
+
     return (
       <Container id="tastingsession-container" className="rounded">
-        <h1>{this.state.tastingSessionName}</h1>
+        <Row id="title-date-row">
+            <Col>
+              <h1 className="ml-4">{this.state.tastingSessionName}</h1>
+            </Col>
+            <Col>
+              <p className="text-right mr-4">{moment(this.state.tastingSessionStartingDate).format(DATE_FORMAT)}</p>
+            </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <h1>
+            Information:
+          </h1>
+        </Row>
+        <Row className="justify-content-center">
+        <div className="border rounded shadow-sm p-3 mb-5 bg-white rounded w-75" align="center">
+            {this.state.tastingSessionAdditionalInfo}
+          </div>
+        </Row>
+        <Row className="justify-content-center">
+          <h1>Drinks:</h1>
+        </Row>
+        <Row className="justify-content-center">
+        <ul className="list-group list-group-flush bg-light w-75">
+          <li className="list-group-item">{beerCard}</li>
+          <li className="list-group-item">{beerCard}</li>
+        </ul>
+        </Row>
       </Container>
     );
   }
