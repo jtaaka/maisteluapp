4d1e87f59816447b7e8914898bdf62d0ad3b4ba2
Joonas Salojärvi <salojarvi.joonas@gmail.com>
2019-04-16 00:37:16 +0300

    Add ability to remove beer

--- src/pages/beers/BeerInfo.jsx
+++ src/pages/beers/BeerInfo.jsx
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import {Container, Row, Alert, Navbar, Nav, NavDropdown} from 'react-bootstrap';
 import { LinkContainer } from "react-router-bootstrap";
+import { withRouter } from 'react-router-dom';
+
+import {notificationSuccess, notificationError} from '../../components/Notification'
 
 import axios from 'axios';
 
@@ -75,7 +78,18 @@ class BeerInfo extends Component {
     }
 
     deleteBeer(){
-
+        axios.delete('beers/' + this.state.beer.id)
+            .then((response) => {
+                if(response.status === 200){
+                    notificationSuccess("Deleted succesfully");
+                    this.props.history.push('/tastingapp/beers/')
+                } else {
+                    notificationError('Response: ' + response.data);
+                }
+                
+            }).catch((error) => {
+                notificationError(error.message);
+            })
     }
 
     modifyBeer(){
@@ -91,11 +105,11 @@ class BeerInfo extends Component {
             <Navbar>
                 <Nav id="borderless" variant="tabs" className="ml-auto" activeKey="1">
                     <NavDropdown title="Manage beer" id="basic-nav-dropdown">
-                    <LinkContainer to="/" exact={true} onClick={this.deleteBeer}>
+                    <LinkContainer to="#" exact={true} onClick={this.deleteBeer}>
                         <NavDropdown.Item>Delete beer</NavDropdown.Item>
                     </LinkContainer>
                     
-                    <LinkContainer to="/" exact={true} onClick={this.modifyBeer}>
+                    <LinkContainer to="#" exact={true} onClick={this.modifyBeer}>
                         <NavDropdown.Item>Modify beer</NavDropdown.Item>
                     </LinkContainer>
                     </NavDropdown>
@@ -124,5 +138,5 @@ class BeerInfo extends Component {
 
 }
 
-export default BeerInfo;
+export default withRouter(BeerInfo);
 
